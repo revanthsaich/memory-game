5c68a2ce3bc1b7c00eefa6a0fe9541541dcaaf29
revanthsaich <revanthsaichaparala05@gmail.com>
2025-01-27 15:25:13 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
 import App from './App.jsx';
-import Home from './pages/Home.jsx';
-import Temp from './pages/Temp.jsx';
-import Game from './pages/Game.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/temp" element={<Temp />} />
-        <Route path="/game" element={<Game />} />
-      </Routes>
-    </Router>
+    <App />
   </StrictMode>
 );
